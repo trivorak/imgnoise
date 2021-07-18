a7dc553f758e5307776e12f0e9de88da07bd0e92
Drake Champagne <trivorak@gmail.com>
2021-07-18 12:50:29 -0500

    long note duration added to trail.py

--- trial.py
+++ trial.py
@@ -1,6 +1,9 @@
 from PIL import Image
 from midiutil import MIDIFile
 
+#Variable Defaults
+countingVar = 1
+
 #Midi Defaults
 track = 0
 channel = 0
@@ -12,15 +15,23 @@ volume = 100
 MyMIDI = MIDIFile(1)
 MyMIDI.addTempo(track,time,tempo)
 
+#Get Color Funtion
+def getColor(w,h):
+	return im.getpixel((w,h))
+
 # Open Image and get size
 im = Image.open('output.png');
 width , height = im.size;
 
 for y in range(0,height):
 	for x in range(0, width):
-		if (im.getpixel((x,y)) < 1):
-			MyMIDI.addNote(track,channel,abs(-127+y),time, duration, volume)
-		time = time + 0.25
+		if (x+1 < width and getColor(x,y) == getColor(x+1,y)):
+			countingVar = countingVar + 1
+			continue
+		if (getColor(x,y) < 1):
+			MyMIDI.addNote(track,channel,abs(-127+y),time, duration*countingVar, volume)
+		time = time + duration*countingVar
+		countingVar = 1
 	time = 0
 
 with open("output.mid","wb") as output_file:
