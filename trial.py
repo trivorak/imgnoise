6c2d05c042a049fb300a8953d8da63d6ce384f97
Drake Champagne <trivorak@gmail.com>
2021-07-18 12:08:49 -0500

    Reworked trial.py to work in the x axis then walk down the y axis. Setting up the code to sustain notes for consecutive notes. See python for continue: reference code

--- trial.py
+++ trial.py
@@ -13,14 +13,15 @@ MyMIDI = MIDIFile(1)
 MyMIDI.addTempo(track,time,tempo)
 
 # Open Image and get size
-im = Image.open('input.png');
+im = Image.open('output.png');
 width , height = im.size;
 
-for y in range(1,width):
-	for x in range(1, height):
-		if (im.getpixel((y,x)) < 128):
-			MyMIDI.addNote(track,channel,abs(-128+x),time, duration, volume)
-	time = time + 0.25
+for y in range(0,height):
+	for x in range(0, width):
+		if (im.getpixel((x,y)) < 1):
+			MyMIDI.addNote(track,channel,abs(-127+y),time, duration, volume)
+		time = time + 0.25
+	time = 0
 
 with open("output.mid","wb") as output_file:
 	MyMIDI.writeFile(output_file)
