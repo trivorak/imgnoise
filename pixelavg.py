8cc1e62b20ae056c62cb2716e749301ca6da62f1
Drake Champagne <trivorak@gmail.com>
2021-07-22 16:18:35 -0500

    Update pixelavg.py
    fixed tabbing

--- pixelavg.py
+++ pixelavg.py
@@ -21,14 +21,13 @@ im = Image.open('output.png');
 width , height = im.size;
 
 for x in range(0,width):
-    for y in range(0, height):
-        colorValue=im.getpixel((x,y))
-        colorValue = 255-colorValue
-        colorValAvg = colorValAvg + colorValue
-    colorValAvg = colorValAvg / (height*2)
-    MyMIDI.addNote(track,channel,math.ceil(colorValAvg/2),time, duration, volume)
-    time = time + 0.25
-    colorValAvg = 0
+	for y in range(0, height):
+		colorValue=im.getpixel((x,y))
+		colorValAvg = colorValAvg + colorValue
+	colorValAvg = colorValAvg / (height*2)
+	MyMIDI.addNote(track,channel,math.ceil(colorValAvg/2),time, duration, volume)
+	time = time + 0.25
+	colorValAvg = 0
 
 with open("pixelavg.mid","wb") as output_file:
-    MyMIDI.writeFile(output_file)
+	MyMIDI.writeFile(output_file)
