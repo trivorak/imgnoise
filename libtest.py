90e85468fea836728592dc562ec05974b390da8c
Drake Champagne <trivorak@gmail.com>
2021-08-01 12:37:35 -0500

    Completed the output of the libtest file. Proof of concept

--- libtest.py
+++ libtest.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from midiutil import MIDIFile
 
 # Define List
 normList = []
@@ -7,7 +8,7 @@ normList = []
 im = Image.open('output.png');
 width , height = im.size;
 
-# Main Loop
+# Main Input Loop
 for y in range(0,width):
 	for x in range(0, height):
 		colorValue=im.getpixel((y,x))
@@ -24,3 +25,29 @@ for element in normList:
 
 myFile.close()
 
+#///////////////////////// 
+# Main Output Loop  //////
+#/////////////////////////
+
+# Define Midi Defaults
+track = 0
+channel = 0
+time = 0
+duration = 0.25
+tempo = 110
+volume = 100
+controllerNumber = 55
+
+# Init Midi File
+MyMIDI = MIDIFile(1)
+MyMIDI.addTempo(track,time,tempo)
+
+# Output translation
+
+for element in normList:
+	MyMIDI.addNote(track,channel,round(element*127),time,duration,volume)
+	time = time + 0.25
+
+# Write Midi file
+with open("libtest.mid","wb") as output_file:
+	MyMIDI.writeFile(output_file)
