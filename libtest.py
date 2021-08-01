e26f6db8df37f28dc8e9540153c85094a9aed519
Drake Champagne <trivorak@gmail.com>
2021-07-31 22:15:26 -0500

    added new file libtest.py to work out a new concept on a modular approach to these concepts

---
+++ libtest.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import math
+
+# Define List
+normList = []
+
+# Open Image and get size
+im = Image.open('output.png');
+width , height = im.size;
+
+# Main Loop
+for y in range(0,width):
+	for x in range(0, height):
+		colorValue=im.getpixel((y,x))
+		colorValue = colorValue / 255
+		normList.append(colorValue)
+
+print(normList)
+
+myFile=open('output.norm','w')
+
+for element in normList:
+	myFile.write(str(element))
+	myFile.write('\n')
+
+myFile.close()
+
