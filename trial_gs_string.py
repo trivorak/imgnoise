954e7dc3e036473fe4a9d250c25823ad18894bf2
Drake Champagne <trivorak@gmail.com>
2021-10-09 00:36:46 -0400

    Added New Image Midi File

--- trial_gs_string.py
+++ trial_gs_string.py
@@ -18,13 +18,13 @@ im = Image.open('output.png');
 width , height = im.size;
 im = im.convert(mode='L')
 
-for y in range(0,width):
-	for x in range(0, height):
+for x in range(0,height):
+	for y in range(0, width):
 		colorValue=im.getpixel((y,x))
 		colorValue = 255-colorValue
 		if (colorValue > 0):
-			MyMIDI.addNote(track,channel,abs(-127+x),time, duration, math.ceil(colorValue/2))
-	time = time + 0.25
+			MyMIDI.addNote(track,channel,math.ceil(colorValue/2),time, duration, volume)
+		time = time + 0.25
 
 with open("output.mid","wb") as output_file:
 	MyMIDI.writeFile(output_file)
