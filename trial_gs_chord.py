f60331faf97fef0075b19601483f233fd0a7519d
Drake Champagne <trivorak@gmail.com>
2023-01-08 16:21:38 -0600

    Added Chord Range

--- trial_gs_chord.py
+++ trial_gs_chord.py
@@ -13,23 +13,26 @@ volume = 100
 MyMIDI = MIDIFile(1)
 MyMIDI.addTempo(track,time,tempo)
 
-# Open Image and get size / Convert to greyscale
+# Open Image and get size
 im = Image.open('output.png');
 width , height = im.size;
 
-#Used for Chord Building Finds Area of Image
-#Divides the Area by number of notes you want in a chord
-
+# Used for Chord Building Finds Area of Image
+# Divides the Area by number of notes you want in a chord
+# Need to add Chord Range
 imageArea = width * height
 loopback = width
 loopdivision = loopback*duration
 
+# Sets Range of Chords. Set OctaveRange and leave RangeMath alone
+octaveRange = 4
+rangeMath = (4*12)/128
 
 for y in range(0,height):
 	for x in range(0, width):
 		colorValue=im.getpixel((x,y))
 		if (colorValue > 0):
-			MyMIDI.addNote(track,channel,math.ceil(colorValue/2),time, duration, volume)
+			MyMIDI.addNote(track,channel,math.ceil(rangeMath*(colorValue/2)),time, duration, volume)
 		time = time + duration
 		if (time > loopdivision):
 			time = 0
